6b6de850edae023ce8f7fa81c609fa20fbb16f2c
Isaac-Vitale <85357842+Isaac-Vitale@users.noreply.github.com>
2022-06-02 09:25:52 -0300

    Add files via upload

---
+++ clase listas js/main.js
@@ -0,0 +1,24 @@
+
+//1)
+//let datos = ["Bad Bunny", "NF", "Rels B"]
+// datos.push("AC/DC")
+// console.log(datos[2])
+
+
+//2)
+// let canciones = ["Tranquila", "Desesperados", "Moscow Mule"]
+// console.log("Canciones en la playlist: ", canciones)
+// document.write("Canciones de la playlist: ", canciones)
+// document.write("Hay", canciones.length, " canciones en la playlist")
+
+//3)
+// let platos = ["Pasta", "Asado", "Tacos"]
+// let paises = ["Italia", "Argentina", "México"]
+// platos.push("Sushi")//agrega un elemento al final de la lista
+// let paises_platos = [platos, paises]
+// platos.pop("Sushi") //"pop" elimina el ultimo elemento del array
+// platos.unshift("Guiso") //Agrega un nuevo elemento al principio de la lista
+// platos.shift("Guiso")//Elimina el primer elemento de la lista
+// document.write(paises_platos[0]) //el segundo corchete accede al index del elemento seleccionado 
+
+
